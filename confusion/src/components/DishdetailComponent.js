74b3dbdd3fbf44c97526b05a2789fa130667813d
Aakash Murugan <aakashmur@gmail.com>
2020-09-22 11:33:55 +0530

    Fetch from Server

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import {Control, LocalForm, Errors} from 'react-redux-form';
 import {Link} from 'react-router-dom';
 //import { addComment } from '../redux/ActionCreators';
 import {Loading} from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -104,7 +105,7 @@ function RenderDish(dish) {
     if (dish != null){
         return(
             <Card>
-                <CardImg width='100%' top src={dish.image} />
+                <CardImg width='100%' top src={baseUrl+dish.image} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
--- confusion/src/components/LoadingComponent.js
+++ confusion/src/components/LoadingComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Loading = () => {
     return (
-        <div classNmae='col-12'>
+        <div className='col-12'>
             <span className='fa fa-spinner fa-pulse fa-3x fa-fw text-primary' />
             <p>Loading...</p>
         </div>
